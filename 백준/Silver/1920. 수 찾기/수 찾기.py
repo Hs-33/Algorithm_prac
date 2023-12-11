55084984dc6cf99b7fc596387be1626b5d0d65fa
Shinjeong Hwang <78021178+Hs-33@users.noreply.github.com>
2023-12-12 00:43:26 +0900

    [Silver IV] Title: 수 찾기, Time: 440 ms, Memory: 50104 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,31 @@
+import sys
+n = int(sys.stdin.readline().strip())
+num_list1 = list(map(int,sys.stdin.readline().strip().split()))
+m = int(sys.stdin.readline().strip())
+num_list2 = list(map(int,sys.stdin.readline().strip().split()))
+
+num_list1 = list(set(num_list1))
+num_list1.sort()
+num_list1_t = num_list1
+
+def binary_search(target, data):
+    start = 0
+    end = len(data) - 1
+
+    while start <= end:
+        mid = (start + end) // 2
+
+        if data[mid] == target:
+            return True 
+        elif data[mid] < target:
+            start = mid + 1
+        else:
+            end = mid -1
+
+    return False
+
+for i in range(len(num_list2)):
+    if binary_search(num_list2[i], num_list1):
+        print(1)
+    else:
+        print(0)
