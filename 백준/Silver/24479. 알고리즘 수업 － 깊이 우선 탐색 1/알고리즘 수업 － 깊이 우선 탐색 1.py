f65becfe558b531056c04184f34bd98051b8e869
Shinjeong Hwang <78021178+Hs-33@users.noreply.github.com>
2024-01-19 18:44:49 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 580 ms, Memory: 140304 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -0,0 +1,34 @@
+import sys
+def dfs(start, graph, n):
+    stack = [start]
+    visit = set()
+    res = [0] * (n + 1)
+    cnt = 1
+
+    while stack:
+        curr = stack.pop()
+
+        if curr not in visit:
+            res[curr] = cnt
+            cnt += 1
+            visit.add(curr)
+
+        for nxt in sorted(graph[curr], reverse=True):
+            if nxt not in visit:
+                stack.append(nxt)
+
+    return res[1:]
+
+n, m, r = map(int, sys.stdin.readline().strip().split())
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b = map(int, sys.stdin.readline().strip().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+# No need to sort each adjacency list, as we're sorting during traversal
+result = dfs(r, graph, n)
+
+for a in result:
+    print(a)
